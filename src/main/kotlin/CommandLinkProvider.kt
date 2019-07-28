375829665fcd7e1a4335acb94118448977c8b7b5
Friedrich von Never <friedrich@fornever.me>
2019-07-28 23:10:30 +0700

    Check if the file is executable before running

--- src/main/kotlin/CommandLinkProvider.kt
+++ src/main/kotlin/CommandLinkProvider.kt
@@ -1,12 +1,17 @@
 package me.fornever.commandlink
 
 import com.intellij.execution.configurations.GeneralCommandLine
+import com.intellij.execution.configurations.PathEnvironmentVariableUtil
 import com.intellij.execution.filters.ConsoleFilterProvider
 import com.intellij.execution.filters.Filter
 import com.intellij.execution.filters.HyperlinkInfo
 import com.intellij.execution.filters.TextConsoleBuilderFactory
 import com.intellij.execution.process.ProcessHandlerFactory
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.SystemInfo
+import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowAnchor
 import com.intellij.openapi.wm.ToolWindowManager
@@ -17,6 +22,10 @@ import java.net.URL
 import java.util.*
 
 class CommandLinkProvider : ConsoleFilterProvider {
+
+    companion object {
+        private val logger = Logger.getInstance(CommandLinkProvider::class.java)
+    }
 
     private var toolWindow: ToolWindow? = null
     private fun initToolWindow(project: Project): ToolWindow {
@@ -47,8 +56,30 @@ class CommandLinkProvider : ConsoleFilterProvider {
             consoleView.attachToProcess(processHandler)
         }
 
+        private fun isExecutable(file: File) = if (SystemInfo.isWindows) {
+            val pathExtensions = PathEnvironmentVariableUtil.getWindowsExecutableFileExtensions()
+            val extensionWithDot = "." + file.extension.toLowerCase(Locale.ROOT)
+            pathExtensions.contains(extensionWithDot)
+        } else {
+            file.canExecute()
+        }
+
         private fun buildHyperlinkInfo(url: String) = HyperlinkInfo {
-            runProgram(project, URLUtil.urlToFile(URL(url)))
+            val file = URLUtil.urlToFile(URL(url))
+            logger.info("Link to file \"$file\" was clicked")
+            if (isExecutable(file)) {
+                logger.info("File \"$file\" is executable, executing")
+                runProgram(project, file)
+            } else {
+                logger.info("File \"$file\" is not executable, opening")
+                val virtualFile = LocalFileSystem.getInstance().findFileByIoFile(file)
+                if (virtualFile == null) {
+                    logger.warn("Virtual file is null for \"$file\"")
+                    return@HyperlinkInfo
+                }
+
+                FileEditorManager.getInstance(project).openFile(virtualFile, true)
+            }
         }
 
         override fun applyFilter(line: String, entireLength: Int): Filter.Result? {
