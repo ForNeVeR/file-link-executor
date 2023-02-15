af819ad5e0a434376406f93a6d0a70672a27998b
Friedrich von Never <friedrich@fornever.me>
2023-02-15 23:06:33 +0100

    Build: update gradle-intellij-plugin to 1.13.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("me.filippov.gradle.jvm.wrapper") version "0.14.0"
     id("org.jetbrains.changelog") version "2.0.0"
-    id("org.jetbrains.intellij") version "1.10.0"
+    id("org.jetbrains.intellij") version "1.13.0"
     id("org.jetbrains.kotlin.jvm") version "1.7.20"
 }
 
