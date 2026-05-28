6e55d41cb9e771aad5dd016b56ed1ca3f631c54e
Friedrich von Never <friedrich@fornever.me>
2026-05-29 00:30:26 +0200

    Build: migrate the settings file to .kts

---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2019 Friedrich von Never <friedrich@fornever.me>
+ * SPDX-FileCopyrightText: 2026 file-link-executor contributors <https://github.com/ForNeVeR/file-link-executor>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+rootProject.name = "file-link-executor"
